c25674fac82bf6ab70a972ae051d12d4fe6cfe9e
IlMeloIl <jgvmelo123@gmail.com>
2024-10-02 12:23:19 -0300

    Finish script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector(".change-grid-btn");
+const container = document.querySelector(".container");
+
+btn.addEventListener("click", changeGrid);
+let gridSize = 2;
+
+function changeGrid(){
+    let gridSize = prompt("Choose grid size, min: 2 and max: 100");
+    gridSize = Number(gridSize);
+
+    if (gridSize >= 2 && gridSize <= 100){
+        container.innerHTML = '';
+        drawGrid(gridSize);
+    } else 
+        alert("minimum 2 and maximum 100");
+    
+}
+
+function drawGrid(gridSize){
+    for (let i = 0; i < gridSize; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 0; j < gridSize; j++){
+            const col = document.createElement("div");
+            row.appendChild(col);
+            col.classList.add("col");
+            col.addEventListener("mouseover", () => {
+                col.style["backgroundColor"] = randomColor();
+            });
+        }
+    }
+}
+
+function randomColor(){
+    return `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`
+}
+
+drawGrid(gridSize);
